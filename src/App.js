3d8490b4df58e10fc85639bb645f886a5e874aeb
bgchoi205 <80654620+bgchoi205@users.noreply.github.com>
2022-06-29 23:23:49 +0900

    useEffect Deps

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
   console.log("rendered!");
+
   const runOnlyOnce = () => {
     console.log("only once!");
   };
   useEffect(runOnlyOnce, []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("Search For ", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    if (counter !== 0) {
+      console.log("Counter is ", counter);
+    }
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== "" && counter !== 0) {
+      console.log("keyword and counter changed!!");
+    }
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        type="text"
+        value={keyword}
+        onChange={onChange}
+        placeholder="Search here"
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"Continue"} />
       <button onClick={onClick}>click me</button>
@@ -25,5 +49,11 @@ function App() {
 // useEffect(() => {
 //   console.log("only once!");
 // }, []);
+
+// 아래처럼 [] 사이에 useState의 keyword를 지정해주면 keyword가 변화할 때만 함수가 실행된다.
+
+// useEffect(() => {
+//   console.log("Search For ", keyword);
+// }, [keyword]);
 
 export default App;
